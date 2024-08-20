857d500ebd72b1ce62b966cacaa88b7386fcc690
xotoverse <xotoverse@gmail.com>
2024-08-20 10:57:40 +0200

    auto

---
+++ variables.tf
@@ -0,0 +1,20 @@
+####################### VARIABLE
+
+variable "region" {}
+variable "environment" {}
+variable "xtcross-account-id" {}
+variable "xtcross-container-portlist" {}
+variable "xtcross-host-portlist" {}
+variable "xtcross-cluster-name" {}
+variable "xtcross-organization" {}
+variable "xtcross-domain-name" {}
+variable "xtcross-service-name" {}
+variable "xtcross-service-version" {}
+variable "xtcross-password" {}
+variable "xtcross-enable-monitor" {}
+variable "xtcross-username" {}
+variable "xtcross-container-definition" {}
+variable "xtcross-healthcheck-pathlist" {}
+variable "xtcross-listener-hostlist" {}
+variable "xtcross-vpn-security" {}
+variable "xtcross-task-memory" { default = 512 }
