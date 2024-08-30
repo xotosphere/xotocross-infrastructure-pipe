1d6d643f3b7e0e606165e6dec72136e2c7d63c2b
Alberto <42894888+Nenino2@users.noreply.github.com>
2024-08-30 18:14:45 +0200

    Fix

--- data.tf
+++ data.tf
@@ -3,7 +3,7 @@
 
 data "aws_vpc" "xtcross-vpc" {
   filter {
-    name = "tag:Name"
+    name   = "tag:Name"
     values = ["${var.prefix}-${var.environment}"]
   }
 }
@@ -11,22 +11,22 @@ data "aws_vpc" "xtcross-vpc" {
 data "aws_security_group" "xtcross-securitygroup" {
   vpc_id = data.aws_vpc.xtcross-vpc.id
   filter {
-    name = "tag:Name"
+    name   = "tag:Name"
     values = ["${var.prefix}-${var.environment}-${var.xtcross-vpn-security == "public" ? "public" : "private"}"]
   }
 }
 
 data "aws_subnets" "xtcross-public-subnetlist" {
   filter {
-    name = "tag:Name"
-    values = ["${var.prefix}-${var.environment}-0", "${var.prefix}-${var.environment}-1"]
+    name   = "tag:Name"
+    values = ["${var.prefix}-${var.environment}-public-0", "${var.prefix}-${var.environment}-public-1"]
   }
 }
 
 data "aws_subnets" "xtcross-private-subnetlist" {
   filter {
-    name = "tag:Name"
-    values = ["${var.prefix}-${var.environment}-0", "${var.prefix}-${var.environment}-1"]
+    name   = "tag:Name"
+    values = ["${var.prefix}-${var.environment}-private-0", "${var.prefix}-${var.environment}-private-1"]
   }
 }
 
