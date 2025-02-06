90c3b4e34a45627b4a5e7b6755fb4be047d40a46
Alberto <halbtale@gmail.com>
2025-02-06 18:19:19 +0100

    fix

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ module "elb" {
   source                             = "github.com/xotosphere/xotocross-infrastructure-ecs//modules/elb"
   environment                        = var.environment
   region                             = var.region
-  xtcross-loadbalancer-name          = "${var.xtcross-cluster-name}-${var.environment}"
+  xtcross-loadbalancer-name          = var.xtcross-cluster-name
   xtcross-host-portlist              = module.fluentbit.xtcross-host-portlist
   xtcross-listener-portlist          = module.fluentbit.xtcross-host-portlist
   xtcross-listener-hostlist          = module.fluentbit.xtcross-listener-hostlist
